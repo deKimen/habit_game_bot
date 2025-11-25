5645e9d157c741a6350b567ec189de31f645be67
evdokimenko.da@phystech.su <evdokimenko.da@phystech.edu>
2025-11-25 12:16:42 +0300

    added new imports

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -7,8 +7,10 @@ This package contains reusable utilities:
 - Common helper functions
 """
 from app.utils.validators import validate_habit_name, validate_xp_value
-
+from app.utils.formatters import format_habits_list, format_today_habits
 __all__ = [
     "validate_habit_name",
     "validate_xp_value",
+    "format_habits_list",
+    "format_today_habits"
 ]
