d601497c9a9825c970911f21cea14132fe6d7594
Евдокименко Дарья Алексеевна <Dashulyaevdokimenko@gmail.com>
2025-12-04 12:02:55 +0300

    Update __init__.py

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -8,9 +8,14 @@ This package contains reusable utilities:
 """
 from app.utils.validators import validate_habit_name, validate_xp_value
 from app.utils.formatters import format_habits_list, format_today_habits
+from app.utils.achievement_formatters import format_achievements_list, format_achievement_unlock, format_achievement
+
 __all__ = [
     "validate_habit_name",
     "validate_xp_value",
     "format_habits_list",
     "format_today_habits"
+    "format_achievements_list",
+    "format_achievement_unlock",
+    "format_achievement"
 ]
