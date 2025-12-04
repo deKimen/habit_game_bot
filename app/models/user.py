496be3786ba17370da3aef0750b5f86b6a4060f0
evdokimenko.da@phystech.su <evdokimenko.da@phystech.edu>
2025-12-04 11:12:41 +0300

    added changes, connected with achievements

--- app/models/user.py
+++ app/models/user.py
@@ -25,7 +25,9 @@ class User(Base):
     # Связи
     character: Mapped["Character"] = relationship("Character", back_populates="user", uselist=False)
     habits: Mapped[List["Habit"]] = relationship("Habit", back_populates="user")
-    
+    achievements: Mapped[List["Achievement"]] = relationship("Achievement",
+                                                             back_populates="user")
+
     @property
     def display_name(self) -> str:
         """
